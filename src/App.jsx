0b126f0893c50dedd93834cd9f052457ac921ed1
aviraj0403 <aviraj0403@gmail.com>
2024-06-26 20:57:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import TodoList1 from './ToDoList1'
+
+function App() {
+  
+
+  return (
+   
+
+   <TodoList1 />
+
+   
+  )
+}
+
+export default App
---
+++ src/ToDoList1.jsx
@@ -0,0 +1,16 @@
+export default function TodoList1()
+{
+    return(
+        <div>
+            <input placeholder="Add New Task"></input> <br></br>
+
+            <br></br><button>Add Task</button>
+            <br></br>  <br></br>  <br></br>
+            <hr></hr>
+            <h4>Task Todo </h4>
+            <ul>
+
+            </ul>
+        </div>
+    )
+}
